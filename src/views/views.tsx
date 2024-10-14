ccd3bcf0b3678bbf49fbddb0658c6fc50d0fdddb
Ofer Hopstone Groman <oferg2@brainpop.com>
2024-10-14 15:39:39 +0300

    Resolving eslint issues

--- src/views/views.tsx
+++ src/views/views.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Navigate, Route, Routes} from 'react-router-dom';
 import {Welcome} from './Welcome/Welcome';
-import {GameOver} from "./GameOver/GameOver.tsx";
+import {GameOver} from './GameOver/GameOver.tsx';
 import GameBoard from '@components/game-board/GameBoard';
 
 export const Views = (): React.JSX.Element => (
     <Routes>
-        <Route path="/" element={<GameBoard className="flex w-full h-full"/>} />
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/game-over" element={<GameOver />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path='/' element={<GameBoard className='flex w-full h-full'/>} />
+        <Route path='/welcome' element={<Welcome />} />
+        <Route path='/game-over' element={<GameOver />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
     </Routes>
 );
